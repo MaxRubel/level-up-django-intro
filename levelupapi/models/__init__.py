c7911f620f09fae7c110093191b95e8645d6e1e1
max rubel <maxrubel1@gmail.com>
2024-05-13 21:23:22 -0500

    first commit

---
+++ levelupapi/models/__init__.py
@@ -0,0 +1,5 @@
+from .gamer import Gamer
+from .event import Event
+from .game import Game
+from .game_type import Game_type
+from .event_gamer import Event_gamer
---
+++ levelupapi/models/event.py
@@ -0,0 +1,10 @@
+from django.db import models
+from .game import Game
+from .gamer import Gamer
+
+class Event (models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    description = models.CharField(max_length=150)
+    date = models.DateField()
+    time = models.TimeField()
+    orgaziner = models.ForeignKey(Gamer, on_delete=models.CASCADE)
